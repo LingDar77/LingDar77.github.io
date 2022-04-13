a14db6970381957aceb7e3bd260711d658dca944
LingDar77 <LingDar77@outlook.com>
2022-04-13 17:13:41 +0800

    5th day

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let hadler1 = EventHandler.BindEvent(".Icon", "click", function ()
+{
+    alert("hello");
+});
+EventHandler.BindEvent(".Icon", "click", function ()
+{
+    alert("world");
+});
+
+let a = document.querySelector("");
